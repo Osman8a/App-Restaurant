517ed441279b8cb2331444465574867c30cf8a90
Osman Ochoa <ochoaosman@gmail.com>
2018-01-23 16:53:28 -0400

    Tab por valoración

--- src/pages/tab-por-valoracion/tab-por-valoracion.ts
+++ src/pages/tab-por-valoracion/tab-por-valoracion.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseDbProvider } from "../../providers/firebase-db/firebase-db";
 
 /**
  * Generated class for the TabPorValoracionPage page.
@@ -14,12 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tab-por-valoracion.html',
 })
 export class TabPorValoracionPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  warmth: number = 1300;
+  sitios: any;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public dbFirebase: FirebaseDbProvider
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabPorValoracionPage');
   }
 
+  ionViewDidEnter() {
+    this.dbFirebase.getSitios().subscribe(sitios => {
+      this.sitios = sitios;
+    });
+  }
 }
