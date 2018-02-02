9f3665a8e0eb6234724e416b9e668c55782ed280
Osman Ochoa <ochoaosman@gmail.com>
2018-02-01 23:59:59 -0400

    Refactorización del código para reutilizar métodos provenietes del provedor timeline

--- src/pages/tab-por-precio/tab-por-precio.ts
+++ src/pages/tab-por-precio/tab-por-precio.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TimelineProvider } from "../../providers/timeline/timeline";
+import { IonicPage } from 'ionic-angular';
 import { FirebaseDbProvider } from "../../providers/firebase-db/firebase-db";
 
 
@@ -7,6 +8,7 @@ import { FirebaseDbProvider } from "../../providers/firebase-db/firebase-db";
 @Component({
   selector: 'page-tab-por-precio',
   templateUrl: 'tab-por-precio.html',
+  providers: [TimelineProvider]
 })
 export class TabPorPrecioPage {
 
@@ -14,19 +16,72 @@ export class TabPorPrecioPage {
   sitios: any;
 
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public dbFirebase: FirebaseDbProvider) {
+    public timeline: TimelineProvider,
+    public dbFirebase: FirebaseDbProvider,
+  ) { }
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////--------------   MENÚ / TIMELINE     ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  cerrarSesion() {
+    //this.auth.logout();
+    this.timeline.cerrarSesion();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabPorPrecioPage');
+  verInfoRestaurante(restaurant) {
+    this.timeline.verInfoRestaurante(restaurant);
   }
 
+  presentImage(myImage) {
+    this.timeline.presentImage(myImage);
+  }
+
+  miUbicacion() {
+    this.timeline.miUbicacion();
+  }
+
+  valorarMenu(restaurant) {
+    this.timeline.valorarMenu(restaurant);
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////--------------      COMPARTIR        ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  whatsapShare(nombre, foto) {
+    this.timeline.whatsapShare(nombre, foto);
+  }
+
+  twitterShare(nombre, foto) {
+    this.timeline.twitterShare(nombre, foto)
+  }
+
+  facebookShare(nombre, foto) {
+    this.timeline.facebookShare(nombre, foto);
+  }
+
+  emailShare(nombre, foto) {
+    this.timeline.emailShare(nombre, foto);
+  }
+
+  instagramShare(nombre, foto) {
+    this.timeline.instagramShare(nombre, foto);
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////-------------- CARGA DE COMPONENTES ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * metodo que inicia en el momento que inicia por
+   * primera vez la aplicación
+   * @memberof HomePage
+   */
   ionViewDidEnter() {
     this.dbFirebase.getSitios().subscribe(sitios => {
       this.sitios = sitios;
     });
+  }
+
+  ionViewDidLoad() {
+    this.timeline.obtenerPosicion();
   }
 
 }
--- src/pages/tab-por-valoracion/tab-por-valoracion.ts
+++ src/pages/tab-por-valoracion/tab-por-valoracion.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { FirebaseDbProvider } from "../../providers/firebase-db/firebase-db";
+import { TimelineProvider } from '../../providers/timeline/timeline';
 
 /**
  * Generated class for the TabPorValoracionPage page.
@@ -13,24 +14,78 @@ import { FirebaseDbProvider } from "../../providers/firebase-db/firebase-db";
 @Component({
   selector: 'page-tab-por-valoracion',
   templateUrl: 'tab-por-valoracion.html',
+  providers: [TimelineProvider]
 })
 export class TabPorValoracionPage {
   warmth: number = 1300;
   sitios: any;
+
   constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public dbFirebase: FirebaseDbProvider
-  ) {
+    public timeline: TimelineProvider,
+    public dbFirebase: FirebaseDbProvider,
+  ) { }
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////--------------   MENÚ / TIMELINE     ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  cerrarSesion() {
+    //this.auth.logout();
+    this.timeline.cerrarSesion();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabPorValoracionPage');
+  verInfoRestaurante(restaurant) {
+    this.timeline.verInfoRestaurante(restaurant);
   }
 
+  presentImage(myImage) {
+    this.timeline.presentImage(myImage);
+  }
+
+  miUbicacion() {
+    this.timeline.miUbicacion();
+  }
+
+  valorarMenu(restaurant) {
+    this.timeline.valorarMenu(restaurant);
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////--------------      COMPARTIR        ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  whatsapShare(nombre, foto) {
+    this.timeline.whatsapShare(nombre, foto);
+  }
+
+  twitterShare(nombre, foto) {
+    this.timeline.twitterShare(nombre, foto)
+  }
+
+  facebookShare(nombre, foto) {
+    this.timeline.facebookShare(nombre, foto);
+  }
+
+  emailShare(nombre, foto) {
+    this.timeline.emailShare(nombre, foto);
+  }
+
+  instagramShare(nombre, foto) {
+    this.timeline.instagramShare(nombre, foto);
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////-------------- CARGA DE COMPONENTES ----------/////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /**
+   * metodo que inicia en el momento que inicia por
+   * primera vez la aplicación
+   * @memberof HomePage
+   */
   ionViewDidEnter() {
     this.dbFirebase.getSitios().subscribe(sitios => {
       this.sitios = sitios;
     });
+  }
+
+  ionViewDidLoad() {
+    this.timeline.obtenerPosicion();
   }
 }
